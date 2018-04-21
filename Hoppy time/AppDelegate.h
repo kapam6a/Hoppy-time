93e0576e585c090e5d39e26766c952c30dc25e9d
Aleksey Yakimenko <a.yakimenko@rambler-co.ru>
2018-04-21 15:52:07 +0300

    Добавил проект.

---
+++ Hoppy time/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Hoppy time
+//
+//  Created by a.yakimenko on 21/04/2018.
+//  Copyright © 2018 Aleksey Yakimenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Hoppy time/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Hoppy time
+//
+//  Created by a.yakimenko on 21/04/2018.
+//  Copyright © 2018 Aleksey Yakimenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
