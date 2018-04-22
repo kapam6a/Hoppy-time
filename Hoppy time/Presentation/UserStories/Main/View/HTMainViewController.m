7cf7d0575e2ac60c6500805dd78882cf3fbfad75
Aleksey Yakimenko <a.yakimenko@rambler-co.ru>
2018-04-22 16:47:28 +0300

    Добавил иконки для таб бара. Добавил зашлушки для Profile и News модулей.

--- Hoppy time/Presentation/UserStories/Main/View/HTMainViewController.m
+++ Hoppy time/Presentation/UserStories/Main/View/HTMainViewController.m
@@ -58,6 +58,10 @@
     return 330;
 }
 
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    [self.tableView deselectRowAtIndexPath:indexPath animated:NO];
+}
+
 #pragma mark - Helpers
 
 - (void)configure {
